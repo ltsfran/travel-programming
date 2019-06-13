306322ece83c5261c78644ea0c64db583608f944
Luis Tupa <luis.tupa@orbis.com.pe>
2019-06-13 10:33:52 -0500

    feat(app): añadiendo componente Imagen y creando api fake

--- app/client/src/views/Medicines/description/state/medicine/services.js
+++ app/client/src/views/Medicines/description/state/medicine/services.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const serviceMedicine = {
   async getData() {
     try {
-      const { data } = await axios.get('http://www.json-generator.com/api/json/get/ceyxJkGvuG?indent=2');
+      const { data } = await axios.get('http://localhost:3001/searchmedicines/I');
       return data;
     } catch(e) {
       throw new Error(e.message);
--- app/client/src/views/Medicines/index.jsx
+++ app/client/src/views/Medicines/index.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { DescriptionFeature } from './description';
+import { Image } from '../../components/Image';
 
 export function Medicines() {
   return (
     <div className="s-content">
       <div className="s-content__input"></div>
       <DescriptionFeature />
+      <Image src="https://www.redaccionmedica.com/images/destacados/tomar-paracetamol-durante-el-embarazo-reduce-la-fertilidad-de-las-hijas-5708_620x368.jpg"/>
       <div>Estás en los Medicamentos</div>
     </div>
   );
